fb3fb134ebe6aceaa9d009dd4d897b057df94198
guadgamero <54513042+guadgamero@users.noreply.github.com>
2019-11-21 18:39:57 -0600

    function to change show results here

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ function init(){
   var click = document.getElementById('entrybutton');
   
   function anotherFunction() {
+    function change(){
+      m = document.getElementById('textoutput');
+      m.innerHTML = entryinput.value;
+    }
   var x = document.getElementById('entryinput');
-  alert('Guad Gamero:' + entryinput);
+  alert('Guad Gamero:' + entryinput.value);
+  x.addEventListener('onclick', change);
   }
   
   click.addEventListener('click', anotherFunction);
